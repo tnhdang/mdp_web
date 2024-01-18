7da89cd087420119f4182a204fef158b30dc1865
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2024-01-18 15:58:01 +0700

    h

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+
+import React from "react";
+
+
+const Header = () => {
+  return (
+    <>
+      <div ></div>
+      </>
+  );
+};
+
+export default Header;
--- src/layouts/PublicLayout.jsx
+++ src/layouts/PublicLayout.jsx
@@ -2,13 +2,14 @@
 
 import React, { useState } from "react";
 
+import Header from "../components/Header/Header";
 import { Outlet } from "react-router-dom";
 
 
 function PublicLayout() {
   return (
     <>
-      
+      <Header/>
       <Outlet />
     
     </>
