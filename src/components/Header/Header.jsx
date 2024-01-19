83423a9dbe555e6cdbb1809b05df40c70fe8c552
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2024-01-19 17:46:39 +0700

    h

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,25 +4,137 @@
 import React from "react";
 import "./style.css"
 import "./HeaderData"
-import { HeaderData } from "./HeaderData";
+import { WhatWeDo, Solutions, Company, Customers, Insights, Partners } from "./HeaderData";
+import { DownOutlined } from '@ant-design/icons';
+import { Dropdown, message, Space, Col, Divider, Row } from 'antd';
 
 const Header = () => {
+
+  const onClick = ({ key }) => {
+    message.info(`Click on item ${key}`);
+  };
+
   return (
-    
-      <div id="header" className="header" >
-            {HeaderData.map((item, index) => {
+    // <Row justify="center" align="top" >
+    //   {HeaderData.map((item, index) => {
+    //     if (index == 0) {
+    //       return (
+    //         <Col span={4}>
+    //           <a>{item.label.toUpperCase()}</a>
+    //           <Row justify="center" align="top" >
+    //             {item.list.map((subitem, subindex) => {
+    //               return (
 
-              return(
-                <div style={{backgroundColor:`${item.color}`}}>
-                  {item.name}
+    //                 <Col span={8}>
+    //                   <a >{subitem.label}</a>
+    //                   {subitem.list.map((minisubitem, minisubindex) => {
+    //                     return (
+    //                       <Row justify="center" align="top" >
+    //                         <a>{minisubitem.label}</a>
+    //                       </Row>
+    //                     )
+    //                   })}
+    //                 </Col>
+    //               )
+    //             })}
+    //           </Row>
+    //         </Col>
+    //       )
+    //     }
+    //   })}
+    // </Row>
 
-                </div>
+
+    // <div className="dropdown">
+    //   {HeaderData.map((item, index) => {
+    //       return (
+    //         <>
+    //           <button className="dropbtn">{item.label.toUpperCase()}</button>
+    //           <div className="dropdown-content" style={{left:"30rem"}}>
+    //             <Row justify="center" align="top" >
+    //               {item.list.map((subitem, subindex) => {
+    //                 return (
+    //                   <Col span={8}>
+    //                     <a >{subitem.label}</a>
+    //                     {subitem.list.map((minisubitem, minisubindex) => {
+    //                       return (
+    //                         <Row justify="center" align="top" >
+    //                           <a>{minisubitem.label}</a>
+    //                         </Row>
+    //                       )
+    //                     })}
+    //                   </Col>
+    //                 )
+    //               })}
+
+    //             </Row>
+    //           </div>
+    //         </>
+    //       )
+
+    //   })}</div>
+
+
+    <>
+      <Row justify="center" align="top" >
+        <Col span={4} className="whatwedo menu">
+          <div className="dropdowndata">
+          <Row justify="center" align="middle" >
+            <h2 className="title">What we do</h2>
+            <DownOutlined />
+          </Row>
+
+          <Row justify="center" align="middle" >
+            {WhatWeDo.map((subitem, subindex) => {
+              return (
+                <Col span={8}>
+                  <a>{subitem.label}</a>
+                  {subitem.list.map((minisubitem, minisubindex) => {
+                    return (
+                      <Row justify="center" align="top" >
+                        <a>{minisubitem.label}</a>
+                      </Row>
+                    )
+                  })}
+                </Col>
               )
-            })
-            }
-        Header
+            })} 
+          </Row>
+          </div>
+        </Col>
+        <Col span={4}>
+          <Row justify="center" align="middle" >
+            <h2>Solutions</h2>
+            <DownOutlined />
+          </Row>
+        </Col>
+        <Col span={4}>
+          <Row justify="center" align="middle" >
+            <h2>Customers</h2>
+            <DownOutlined />
+          </Row>
+        </Col>
+        <Col span={4}>
+          <Row justify="center" align="middle" >
+            <h2>Partners</h2>
+            <DownOutlined />
+          </Row>
+        </Col>
+        <Col span={4}>
+          <Row justify="center" align="middle" >
+            <h2>Company</h2>
+            <DownOutlined />
+          </Row>
+        </Col>
+        <Col span={4}>
+          <Row justify="center" align="middle" >
+            <h2>Insights</h2>
+            <DownOutlined />
+          </Row>
+        </Col>
 
-      </div>
+      </Row>
+    </>
 
   );
 };
--- src/components/Header/HeaderData.js
+++ src/components/Header/HeaderData.js
@@ -1,47 +1,488 @@
 export const HeaderData = [
     {
-        name: "What we do",
+        label: "What we do",
         list: [
             {
-                name: "Composable approach",
+                label: "Technology",
+                list: [
+                    {
+                        label: "Technology",
+                    },
+                    {
+                        label: "Composable approach",
+                    },
+                    {
+                        label: "Multi-cloud approach",
+                    },
+                    {
+                        label: "Process orchestration"
+                    },
+                    {
+                        label: "Configuration as Code"
+                    },
+                    {
+                        label: "Security & compliance"
+                    },
+                ],
             },
             {
-                name: "Multi-cloud approach",
+                label: "Service",
+                list: [
+                    {
+                        label: "Enablement"
+                    },
+                    {
+                        label: "Enablement"
+                    },
+                ]
             },
             {
-                name: "Process orchestration"
+                label: "Ecosystem",
+                list: [
+                    {
+                        label: "Advisory"
+                    },
+                    {
+                        label: "Marketplace"
+                    },
+                    {
+                        label: "Developers"
+                    }
+                ]
             },
-            {
-                name: "Configuration as Code"
-            },
-            {
-                name: "Security & compliance"
-            },
-            {
-                name: "Enablement"
-            },
-            {
-                name: "Enablement"
-            },
-            {
-                name: "Advisory"
-            },
-            {
-                name: "Marketplace"
-            },
-            {
-                name: "Developers"
-            }
         ],
     },
     {
-        name: "contact",
-        color: "blue",
+        label: "Solutions",
         list: [
             {
-                name: "b",
-                color: "red"
-            }
+                label: "Lending solutions",
+                list: [
+                    {
+                        label: "Lending",
+                    },
+                    {
+                        label: "SME lennding",
+                    },
+                    {
+                        label: "Retail mortgages"
+                    },
+                    {
+                        label: "Personal lending"
+                    },
+                    {
+                        label: "Buy Now Pay Later"
+                    },
+                    {
+                        label: "Purchase finance"
+                    },
+                ],
+            },
+            {
+                label: "Daily banking solution",
+                list: [
+                    {
+                        label: "Deposits"
+                    },
+                    {
+                        label: "Stored value accounts"
+                    },
+                    {
+                        label: "Business banking & deposits"
+                    },
+                    {
+                        label: "Personal banking & deposits"
+                    },
+                ]
+            },
+            {
+                label: "Overarching solutions",
+                list: [
+                    {
+                        label: "Embedded finance"
+                    },
+                    {
+                        label: "Banking-as-a-Service"
+                    },
+                ]
+            },
         ],
-    }
+    },
+    {
+        label: "Partners",
+        list: [
+            {
+                label: "Partnership",
+                list: [
+                    {
+                        label: "Partner ecosystem",
+                    },
+                    {
+                        label: "Integrated solutions",
+                    },
+                    {
+                        label: "Technology partners"
+                    },
+                    {
+                        label: "Consulting partners & system integrators"
+                    },
+                ],
+            },
+            {
+                label: "Partner programme",
+                list: [
+                    {
+                        label: "Partner with Mambu"
+                    },
+                    {
+                        label: "Partner portal"
+                    },
+                ]
+            },
+        ],
+    },
+    {
+        label: "Company",
+        list: [
+            {
+                label: "BECOME A MAMBUVIAN",
+                list: [
+                    {
+                        label: "Your career",
+                    },
+                    {
+                        label: "Open positions",
+                    },
+                ],
+            },
+            {
+                label: "OUR STORY",
+                list: [
+                    {
+                        label: "About us"
+                    },
+                    {
+                        label: "Our offices"
+                    },
+                ]
+            },
+            {
+                label: "SUSTAINABILITY",
+                list: [
+                    {
+                        label: "Sustainability"
+                    },
+                    {
+                        label: "People, diversity and inclusion"
+                    },
+                ]
+            },
+        ],
+    },
+    {
+        label: "Solutions",
+        list: [
+            {
+                label: "INSIGHTS HUB",
+                list: [
+                    {
+                        label: "Articles",
+                    },
+                    {
+                        label: "Events",
+                    },
+                    {
+                        label: "Webinars"
+                    },
+                    {
+                        label: "Reports"
+                    },
+                    {
+                        label: "Podcasts"
+                    },
+                    {
+                        label: "Whitepapers"
+                    },
+                    {
+                        label: "All insights"
+                    },
+                ],
+            },
+        ],
+    },
+
 ];
+
+
+
+export const HeaderData2 = [
+    {
+        label: "Solutions",
+        list: [
+            {
+                label: "Daily banking solution",
+                list: [
+                    {
+                        label: "Deposits"
+                    },
+                    {
+                        label: "Stored value accounts"
+                    },
+                    {
+                        label: "Business banking & deposits"
+                    },
+                    {
+                        label: "Personal banking & deposits"
+                    },
+                ]
+            },
+            {
+                label: "Overarching solutions",
+                list: [
+                    {
+                        label: "Embedded finance"
+                    },
+                    {
+                        label: "Banking-as-a-Service"
+                    },
+                ]
+            },
+        ],
+    },]
+
+export const WhatWeDo = [
+
+    {
+        label: "Technology",
+        list: [
+            {
+                label: "Technology",
+            },
+            {
+                label: "Composable approach",
+            },
+            {
+                label: "Multi-cloud approach",
+            },
+            {
+                label: "Process orchestration"
+            },
+            {
+                label: "Configuration as Code"
+            },
+            {
+                label: "Security & compliance"
+            },
+        ],
+    },
+    {
+        label: "Service",
+        list: [
+            {
+                label: "Enablement"
+            },
+            {
+                label: "Enablement"
+            },
+        ]
+    },
+    {
+        label: "Ecosystem",
+        list: [
+            {
+                label: "Advisory"
+            },
+            {
+                label: "Marketplace"
+            },
+            {
+                label: "Developers"
+            }
+        ]
+    },
+]
+
+export const Solutions = [
+    {
+        label: "Lending solutions",
+        list: [
+            {
+                label: "Lending",
+            },
+            {
+                label: "SME lennding",
+            },
+            {
+                label: "Retail mortgages"
+            },
+            {
+                label: "Personal lending"
+            },
+            {
+                label: "Buy Now Pay Later"
+            },
+            {
+                label: "Purchase finance"
+            },
+        ],
+    },
+    {
+        label: "Daily banking solution",
+        list: [
+            {
+                label: "Deposits"
+            },
+            {
+                label: "Stored value accounts"
+            },
+            {
+                label: "Business banking & deposits"
+            },
+            {
+                label: "Personal banking & deposits"
+            },
+        ]
+    },
+    {
+        label: "Overarching solutions",
+        list: [
+            {
+                label: "Embedded finance"
+            },
+            {
+                label: "Banking-as-a-Service"
+            },
+        ]
+    },
+]
+
+export const Partners = [
+    {
+        label: "Partnership",
+        list: [
+            {
+                label: "Partner ecosystem",
+            },
+            {
+                label: "Integrated solutions",
+            },
+            {
+                label: "Technology partners"
+            },
+            {
+                label: "Consulting partners & system integrators"
+            },
+        ],
+    },
+    {
+        label: "Partner programme",
+        list: [
+            {
+                label: "Partner with Mambu"
+            },
+            {
+                label: "Partner portal"
+            },
+        ]
+    },
+]
+
+export const Company = [
+    {
+        label: "BECOME A MAMBUVIAN",
+        list: [
+            {
+                label: "Your career",
+            },
+            {
+                label: "Open positions",
+            },
+        ],
+    },
+    {
+        label: "OUR STORY",
+        list: [
+            {
+                label: "About us"
+            },
+            {
+                label: "Our offices"
+            },
+        ]
+    },
+    {
+        label: "SUSTAINABILITY",
+        list: [
+            {
+                label: "Sustainability"
+            },
+            {
+                label: "People, diversity and inclusion"
+            },
+        ]
+    },
+]
+
+export const Customers = [
+    {
+        label: "OUR CUSTOMERS",
+        list: [
+            {
+                label: "Success stories",
+            },
+        ],
+    },
+    {
+        label: "WHO WE SERVE",
+        list: [
+            {
+                label: "Traditional banks"
+            },
+            {
+                label: "Neobanks"
+            },
+            {
+                label: "Lenders"
+            },
+            {
+                label: "Fintechs"
+            },
+            {
+                label: "Telcos"
+            },
+            {
+                label: "eCommerce"
+            },
+        ]
+    },
+
+]
+
+export const Insights = [
+    {
+        label: "INSIGHTS HUB",
+        list: [
+            {
+                label: "Articles",
+            },
+            {
+                label: "Events",
+            },
+            {
+                label: "Webinars"
+            },
+            {
+                label: "Reports"
+            },
+            {
+                label: "Podcasts"
+            },
+            {
+                label: "Whitepapers"
+            },
+            {
+                label: "All insights"
+            },
+        ],
+    },
+]
