241979c16e14f5f4a7d2d40792e56268ab402823
Lưu Lạc Ling <nguyenvantay061999@gmail.com>
2024-01-24 09:53:50 +0700

    Tay-NV update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,5 @@
 /** @format */
 
-<<<<<<< HEAD
 export { default as HomePage } from "./HomePage/HomePage";
 export { default as OurPlatform } from "./OurPlatform/OurPlatform";
-=======
-export {default as HomePage} from './HomePage/HomePage';
-export {default as Lending} from './Solutions/Lending/Lending';
- 
->>>>>>> d833d9b3e0d44b266cf1399216728b7170d1f1b2
+export { default as Lending } from "./Solutions/Lending/Lending";
