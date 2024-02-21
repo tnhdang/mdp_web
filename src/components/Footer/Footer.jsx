f23fb11a855946347829991c3e541216cf892fa2
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2024-02-21 16:33:24 +0700

    h

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -13,8 +13,10 @@ const Footer = () => {
             <Row justify="start" align="middle">
             <p className="font-bold text-[1.1rem]">Location: </p>
             </Row>
+            
             <p className="text-start pl-20 leading-9"> 100 Đ. Lê Lai, Phường Phạm Ngũ Lão, Quận 1, Thành phố Hồ Chí Minh, Việt Nam</p>
-            <p className="text-start pl-20 leading-9"> 39 Bạch Đằng, Phường 2, Tân Bình, Thành phố Hồ Chí Minh 736464, Việt Nam</p>
+            <p className="text-start pl-20 leading-9"> 39 Bạch Đằng, Phường 2, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam</p>
+            <p className="text-start pl-20 leading-9"> 24 P. Tôn Đức Thắng, Cát Linh, Đống Đa, Hà Nội, Việt Nam</p>
             <Row justify="start" align="middle">
             <p className="font-bold text-[1.1rem] pt-5">Email contact: </p>
             </Row>
