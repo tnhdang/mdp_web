8691619369496c3a34f127912ea5dd68c0358040
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2024-02-27 17:37:21 +0700

    h

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -6,17 +6,17 @@ import React from "react";
 import "./style.css";
 const Banner = () => {
   return (
-    <div className="w-full flex items-center align-middle justify-center">
-      <div className="pt-2 banner w-[50%] h-[90vh] pvmax:max-lg:h-[90vh] flex pvmax:max-lg:flex-col pvmax:max-lg:align-middle justify-center  ">
+    <div className="">
+      <div className="pt-2 banner w-[100%] h-[90vh] pvmax:max-lg:h-[90vh] flex pvmax:max-lg:flex-col pvmax:max-lg:align-middle justify-center  ">
       
 
-      <div className="flex flex-col w-[50%] h-[100%] justify-center pvmax:max-lg:w-[100%] pvmax:max-lg:pr-[1rem] pvmax:max-lg:h-[50%] ">
+      <div className="flex flex-col w-[35%] h-[100%] justify-center pl-[1rem] pvmax:max-lg:w-[100%] pvmax:max-lg:pr-[1rem] pvmax:max-lg:h-[50%] ">
         <img src="https://img.freepik.com/premium-photo/finance-banking-concept-financial-services-atm-money-turnover-investment-storage-funds-cash-vaults-bank-successful-investment-productivity-busines_771426-9240.jpg?size=626&ext=jpg&ga=GA1.1.1847015774.1708397240&semt=sph" className="h-[60%] rounded-[3rem] "/>
       </div>
 
       
 
-      <div className="flex flex-col w-[50%] pvmax:max-lg:w-[100%] h-[100%] pvmax:max-lg:h-[50%] gap-20 pvmax:max-lg:gap-10 text-start text-[1.1rem] justify-center ml-[10rem] pvmax:max-lg:ml-0 pvmax:max-lg:px-8 banner-content">
+      <div className="flex flex-col w-[40%] pvmax:max-lg:w-[100%] h-[100%] pvmax:max-lg:h-[50%] gap-20 pvmax:max-lg:gap-10 text-start text-[1.1rem] justify-center ml-[10rem] pvmax:max-lg:ml-0 pvmax:max-lg:px-8 banner-content">
         <p className="font-bold">
           MDP specializes in providing comprehensive business transformation consulting, 
           system integration, and Banking as a Service (BaaS) solutions tailored to meet the 
