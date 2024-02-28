7bba6c946379bcf7666a39230642935e24101353
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2024-02-28 15:21:08 +0700

    h

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import {WhatWeDo, Company, Insights} from "../Header/HeaderData";
+import { WhatWeDo, Company, Insights } from "../Header/HeaderData";
 import { Col, Row } from "antd";
 import logo from "../Header/logo.png";
 
@@ -8,64 +8,30 @@ const Footer = () => {
     return (
         <div className="container">
             <Row justify="start" align="middle">
-            <img className="logo" src={logo} alt="logo" />
+                <img className="logo" src={logo} alt="logo" />
             </Row>
             <Row justify="start" align="middle">
-            <p className="font-bold text-[1.1rem]">Location: </p>
+                <p className="font-bold text-[1.1rem]">Location: </p>
             </Row>
-            
-            {/* <p className="text-start pl-20 leading-9"> * 100 Đ. Lê Lai, Phường Phạm Ngũ Lão, Quận 1, Thành phố Hồ Chí Minh, Việt Nam</p>
-            <p className="text-start pl-20 leading-9"> * 39 Bạch Đằng, Phường 2, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam</p>
-            <p className="text-start pl-20 leading-9"> * 24 P. Tôn Đức Thắng, Cát Linh, Đống Đa, Hà Nội, Việt Nam</p> */}
 
-<Row justify="start" align="top">
-            <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-            <p className="text-start pl-20 leading-9"> <span className="hidden pvmax:max-lg:inline">* </span> 100 Đ. Lê Lai, Phường Phạm Ngũ Lão, Quận 1, Thành phố Hồ Chí Minh, Việt Nam</p>
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-            <p className="text-start pl-20 leading-9"><span className="hidden pvmax:max-lg:inline">* </span> 39 Bạch Đằng, Phường 2, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam</p>
-            </Col>
-            <Col xs={24} sm={24} md={24} lg={8} xl={8}>
-            <p className="text-start pl-20 leading-9"><span className="hidden pvmax:max-lg:inline">* </span> 24 P. Tôn Đức Thắng, Cát Linh, Đống Đa, Hà Nội, Việt Nam</p>
-            </Col>
-</Row>
+            <Row justify="start" align="top">
+                <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+                    <p className="text-start pl-20 leading-9"> <span className="hidden pvmax:max-lg:inline">* </span> 100 Đ. Lê Lai, Phường Phạm Ngũ Lão, Quận 1, Thành phố Hồ Chí Minh, Việt Nam</p>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+                    <p className="text-start pl-20 leading-9"><span className="hidden pvmax:max-lg:inline">* </span> 39 Bạch Đằng, Phường 2, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam</p>
+                </Col>
+                <Col xs={24} sm={24} md={24} lg={8} xl={8}>
+                    <p className="text-start pl-20 leading-9"><span className="hidden pvmax:max-lg:inline">* </span> 24 P. Tôn Đức Thắng, Cát Linh, Đống Đa, Hà Nội, Việt Nam</p>
+                </Col>
+            </Row>
 
             <Row justify="start" align="middle">
-            <p className="font-bold text-[1.1rem] pt-5">Email contact: </p>
+                <p className="font-bold text-[1.1rem] pt-5">Email contact: </p>
             </Row>
-            <p className="text-start pl-20 leading-9 underline"><a  href="mailto: sales@mdpgroup.digital" className="underline">sales@mdpgroup.digital</a></p>
-            
+            <p className="text-start pl-20 leading-9 underline"><a href="mailto: sales@mdpgroup.digital" className="underline">sales@mdpgroup.digital</a></p>
+
         </div>
-
-
-
-
-
-
-// <div className="container">
-//             <Row justify="center" align="middle" >
-                
-
-//                 <Col xs={24} sm={24} md={24} lg={6} xl={6} className="" >
-//                     <img className=" w-[80%]" src={logo} alt="logo" />
-//                 </Col>
-//                 <Col xs={24} sm={24} md={24} lg={18} xl={18} className="pl-20 pvmax:max-lg:pl-0 pvmax:max-md:pl-0" >
-//                     <Row justify="start" align="middle">
-//                         <p className="font-bold text-[1.1rem]">Location: </p>
-//                     </Row>
-
-//                     <p className="text-start pl-20  leading-9"> * 100 Đ. Lê Lai, Phường Phạm Ngũ Lão, Quận 1, Thành phố Hồ Chí Minh, Việt Nam</p>
-//                     <p className="text-start pl-20 leading-9"> * 39 Bạch Đằng, Phường 2, Tân Bình, Thành phố Hồ Chí Minh, Việt Nam</p>
-//                     <p className="text-start pl-20 leading-9"> * 24 P. Tôn Đức Thắng, Cát Linh, Đống Đa, Hà Nội, Việt Nam</p>
-//                     <Row justify="start" align="middle">
-//                         <p className="font-bold text-[1.1rem] pt-5">Email contact: </p>
-//                     </Row>
-//                     <p className="text-start pl-20 leading-9 underline"><a href="mailto: sales@mdpgroup.digital" className="underline">sales@mdpgroup.digital</a></p>
-//                 </Col>
-//             </Row>
-
-
-//         </div>
     );
-    }
+}
 export default Footer;
